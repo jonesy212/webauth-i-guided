b1d34032619e4f6facacc98d2b21278b380f0e8d
Marcus Jones <indelibleink84@gmail.com>
2019-10-14 12:56:52 -0500

    updating auth

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+
+  //validate the user 
+
+  //hash the password
+
+  // we override the password with the hash
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -27,13 +35,16 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+
+//localhost:5000/api/login
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
+  if(username && password){
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bycrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -42,21 +53,53 @@ server.post('/api/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+  }else{
+    res.status(400).json({message: 'please proivde credentials'});
+  }
 });
 
-server.get('/api/users', (req, res) => {
-  Users.find()
+server.get('/api/users', protected, (req, res) => {
+  
+  let {username, password} = req.headers;
+
+  if (username && password){
+  Users.find({username})
     .then(users => {
-      res.json(users);
-    })
+      if(user && bycrypt.compareSync(password, users.password)){
+        res.json(users);
+      } else {
+        res.status(401).json({message: 'Invalid Credentials'});
+      }
+    });
+  }else{
     .catch(err => res.send(err));
-});
+  }
+})
+
+
 
 server.get('/hash', (req, res) => {
+  //npmjs.com/package/bycryptsjs
   // read a password from the Authorization header
+  const password = req.headers.authorization;
+
+  // tht 8 is how we slow down attackers trying to pre-genera hashes
+const hash = bycrypt.hashSync(password, 8);// the 8 is the number of round 2^8
+//another good stargin value is 14
+
+
+if (password ) {
+res.status(200).json({hash});
   // return an object with the password hashed using bcryptjs
   // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+} else{
+  res.status(400).json({message})
+  }
+});
 
+function protected(){
+
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
